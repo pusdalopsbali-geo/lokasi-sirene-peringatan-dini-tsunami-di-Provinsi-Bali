c48899e02188ce3eb42c114ae4f81af8209e974f
pusdalopsbali-geo <pusdalopsbali@gmail.com>
2025-08-17 09:17:32 +0800

    Add files via upload

---
+++ data/RencanaSireneTsunamiIDRIP_5.js
@@ -0,0 +1,13 @@
+var json_RencanaSireneTsunamiIDRIP_5 = {
+"type": "FeatureCollection",
+"name": "RencanaSireneTsunamiIDRIP_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ID_SIRINE": null, "NAMA": "SD N 1 Air Kuning", "DESA": "Air Kuning", "KECAMATAN": "Jembrana", "KABUPATEN": "Jembrana", "X": 114.64985, "Y": -8.402, "Rencana": "Tidak Jadi" }, "geometry": { "type": "Point", "coordinates": [ 114.649846, -8.402001 ] } },
+{ "type": "Feature", "properties": { "ID_SIRINE": null, "NAMA": "SD N 1 Perancak", "DESA": "Perancak", "KECAMATAN": "Jembrana", "KABUPATEN": "Jembrana", "X": 114.61966, "Y": -8.40364, "Rencana": "Tidak Jadi" }, "geometry": { "type": "Point", "coordinates": [ 114.619658, -8.403636 ] } },
+{ "type": "Feature", "properties": { "ID_SIRINE": null, "NAMA": "Kantor Desa", "DESA": "Yeh Kuning", "KECAMATAN": "Yeh Kuning", "KABUPATEN": "Jembrana", "X": 114.65739, "Y": -8.39471, "Rencana": "Jadi" }, "geometry": { "type": "Point", "coordinates": [ 114.657387, -8.394707 ] } },
+{ "type": "Feature", "properties": { "ID_SIRINE": null, "NAMA": "Kantor Desa Delod Berawah", "DESA": "Delod Berawah", "KECAMATAN": "Mendoyo", "KABUPATEN": "Jembrana", "X": 114.67904, "Y": -8.39214, "Rencana": "Tidak Jadi" }, "geometry": { "type": "Point", "coordinates": [ 114.679039, -8.392142 ] } },
+{ "type": "Feature", "properties": { "ID_SIRINE": null, "NAMA": "Kantor Desa Penyaringan", "DESA": "Penyaringan", "KECAMATAN": "Mendoyo", "KABUPATEN": "Jembrana", "X": 114.70875, "Y": -8.37654, "Rencana": "Jadi" }, "geometry": { "type": "Point", "coordinates": [ 114.708747, -8.376535 ] } },
+{ "type": "Feature", "properties": { "ID_SIRINE": null, "NAMA": "Balai Banjar Ketapang Muara", "DESA": "Pengambengan", "KECAMATAN": "Negara", "KABUPATEN": "Jembrana", "X": 114.59048, "Y": -8.39843, "Rencana": "Jadi" }, "geometry": { "type": "Point", "coordinates": [ 114.590483, -8.398425 ] } }
+]
+}
---
+++ data/SireneBTEWSMenaraEksisting_3.js
@@ -0,0 +1,16 @@
+var json_SireneBTEWSMenaraEksisting_3 = {
+"type": "FeatureCollection",
+"name": "SireneBTEWSMenaraEksisting_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Lokasi": "BTDC NUSA DUA" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 115.228328635342777, -8.799930086018145 ] ] } },
+{ "type": "Feature", "properties": { "id": "2", "Lokasi": "TANJUNG BENOA" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 115.221522749568436, -8.756659426693806 ] ] } },
+{ "type": "Feature", "properties": { "id": "3", "Lokasi": "SEMINYAK" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 115.161800581217165, -8.697402252126009 ] ] } },
+{ "type": "Feature", "properties": { "id": "4", "Lokasi": "KUTA" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 115.169842217757747, -8.722749503782261 ] ] } },
+{ "type": "Feature", "properties": { "id": "5", "Lokasi": "KEDONGANAN" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 115.168770770932369, -8.756228295655731 ] ] } },
+{ "type": "Feature", "properties": { "id": "6", "Lokasi": "SANUR" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 115.260741637416345, -8.669362959290217 ] ] } },
+{ "type": "Feature", "properties": { "id": "7", "Lokasi": "SERANGAN" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 115.232892697980859, -8.725588124238431 ] ] } },
+{ "type": "Feature", "properties": { "id": "8", "Lokasi": "SERIRIT" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 114.943031908877956, -8.190815776871602 ] ] } },
+{ "type": "Feature", "properties": { "id": "9", "Lokasi": "TANAH LOT" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 115.085697772794234, -8.615707708186299 ] ] } }
+]
+}
